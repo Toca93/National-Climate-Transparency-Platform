a878f137760fb73419617c5d4afec0627d2db201
Toca93 <todorvujic@gmail.com>
2025-11-26 16:37:37 +0100

    Adding new page

--- web/src/Pages/TestPage/TestPage.tsx
+++ web/src/Pages/TestPage/TestPage.tsx
@@ -8,7 +8,7 @@ const TestPage = () => {
   const [foodList, setFoodList] = useState([]);
   const [form] = Form.useForm();
 
-  const baseUrl = process.env.REACT_APP_BACKEND || 'http://localhost:9100';
+  const baseUrl = process.env.REACT_APP_BACKEND || 'http://localhost:9000';
 
   // 1. FETCH DATA (GET)
   const fetchFood = async () => {
